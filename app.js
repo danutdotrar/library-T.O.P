f14ce5eae90e72ecca98c4f3bf26c67ce4abdbf3
danutdotrar <morarasu.danut@gmail.com>
2023-02-23 14:33:53 +0200

    Add removeCurrentBook function

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const addBook = document.getElementById("add-book");
 const hiddenRead = document.getElementById("hidden");
 const container = document.getElementById("container");
+const formContainer = document.getElementById("form-container");
+const card = document.getElementById("card");
 
 let myLibrary = [];
+let bookIndex;
+let content;
+let read;
 
 // Functions
 // Constructor Book
@@ -19,12 +24,6 @@ function Book(title, author, pages, read = true) {
     };
 }
 
-// Testing books
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", false);
-const meditations = new Book("Meditations", "Marcus Aurelius", "200", true);
-
-// console.log(theHobbit.info());
-
 // Add book to library
 function addBookToLibrary(e) {
     // Prevent form from submitting
@@ -33,7 +32,6 @@ function addBookToLibrary(e) {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
-    let read;
 
     // Find if it is read or not
     let readCheckbox = Array.from(document.getElementsByName("read")).find(
@@ -43,35 +41,58 @@ function addBookToLibrary(e) {
     // Set the read to true or false based on readCheckbox
     if (readCheckbox) {
         readCheckbox.value == "true" ? (read = true) : (read = false);
-        hiddenRead.classList.add("hidden");
 
-        console.log(read);
+        hiddenRead.classList.add("hidden");
     } else {
         hiddenRead.classList.remove("hidden");
     }
 
     myLibrary.push(new Book(title, author, pages, read));
 
-    console.log(myLibrary);
-
     // Loop array
     for (let i in myLibrary) {
         // Create a card for each book added
-        const content = `
-        <div class="card">
-        <div class="info library-title">${title}</div>
-        <div class="info library-author">${author}</div>
-        <div class="info library-pages">${pages}</div>
-        <div class="info library-read">${read ? "Read :)" : "Not read :("}</div>
-    </div>
+        bookIndex = i;
+        content = `
+        <div class="card" id="card" data-book="${bookIndex}">
+            <div class="info library-title">${title}</div>
+            <div class="info library-author">${author}</div>
+            <div class="info library-pages">${pages}</div>
+            <div class="info library-read">${
+                read ? "Read :)" : "Not read :("
+            }</div>
+            <button class="remove" id="remove">Remove book</button>
+        </div>
         `;
+    }
+    // Add content to container
+    container.insertAdjacentHTML("beforeend", content);
+    formContainer.reset();
+}
 
-        container.innerHTML += content;
-        console.log(myLibrary[i].info());
+// Remove current book from the array and DOM
+// Use event delegation - add event listener to the parent element
+function removeCurrentBook(event) {
+    // If the target element is the div that we want, execute
+    if (event.target.matches(".remove")) {
+        // Select the book card, use 'closest' to select the closest ancestor element with the class 'card', which will be the specific card that was clicked.
+        const card = event.target.closest(".card");
+        // Get data-book value
+        const dataBookValue = card.dataset.book;
+
+        // Find the current book index
+        for (let i = 0; i < myLibrary.length; i++) {
+            if ([i] == dataBookValue) {
+                // Remove index book from library
+                myLibrary.splice(i, 1);
+
+                // Remove card from DOM
+                card.remove();
+            }
+        }
     }
 }
 
-// myLibrary.push(theHobbit, meditations);
-
 // Event Listeners
 addBook.addEventListener("click", addBookToLibrary);
+container.addEventListener("click", removeCurrentBook);
