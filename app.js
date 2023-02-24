1e07860965688d96c47d58499cdc18b8149cb29b
danutdotrar <morarasu.danut@gmail.com>
2023-02-24 15:44:43 +0200

    Add modal functions

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const addBook = document.getElementById("add-book");
-const hiddenRead = document.getElementById("hidden");
+const hidden = document.getElementById("hidden");
 const container = document.getElementById("container");
 const formContainer = document.getElementById("form-container");
 const card = document.getElementById("card");
+
+// Modal
 const closeModal = document.getElementById("close");
 const openModal = document.getElementById("open-modal");
 const modal = document.getElementById("modal");
 
+// Select input id's
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let pages = document.getElementById("pages");
+const smallHidden = document.getElementById("read-hidden");
+
 let myLibrary = [];
 let bookIndex;
 let content;
-let read;
+let readAnswer;
 
 // Functions
 // Constructor Book
-function Book(title, author, pages, read = true) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -32,25 +40,35 @@ function addBookToLibrary(e) {
     // Prevent form from submitting
     e.preventDefault();
     // Take in the inputs from the form
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let pages = document.getElementById("pages").value;
 
-    // Find if it is read or not
+    myLibrary.push(
+        new Book(title.value, author.value, pages.value, readAnswer)
+    );
+
+    // Check required fields
+    checkRequired([title, author, pages]);
+
+    // Find if read or not
     let readCheckbox = Array.from(document.getElementsByName("read")).find(
         (read) => read.checked
     );
 
-    // Set the read to true or false based on readCheckbox
-    if (readCheckbox) {
-        readCheckbox.value == "true" ? (read = true) : (read = false);
-
-        hiddenRead.classList.add("hidden");
-    } else {
-        hiddenRead.classList.remove("hidden");
+    // Check if read or not
+    if (!readCheckbox) {
+        smallHidden.classList.remove("hidden");
+        return false;
+    } else if (readCheckbox.value == "true") {
+        readAnswer = true;
+        smallHidden.classList.add("hidden");
+    } else if (readCheckbox.value == "false") {
+        readAnswer = false;
+        smallHidden.classList.add("hidden");
     }
 
-    myLibrary.push(new Book(title, author, pages, read));
+    // If the inputs values are null, stop function
+    if (title.value == "" || author.value == "" || pages.value == "") {
+        return false;
+    }
 
     // Loop array
     for (let i in myLibrary) {
@@ -58,13 +76,13 @@ function addBookToLibrary(e) {
         bookIndex = i;
         content = `
         <div class="card" id="card" data-book="${bookIndex}">
-            <div class="info library-title">${title}</div>
-            <div class="info library-author">${author}</div>
-            <div class="info library-pages">${pages}</div>
-            <div class="info library-read">${
-                read ? "Read :)" : "Not read :("
-            }</div>
-            <button class="remove" id="remove">Remove book</button>
+        <div class="info library-title">${title.value}</div>
+        <div class="info library-author">${author.value}</div>
+        <div class="info library-pages">${pages.value}</div>
+        <div class="info library-read">${
+            readAnswer ? "Read :)" : "Not read :("
+        }</div>
+        <button class="remove" id="remove">Remove book</button>
         </div>
         `;
     }
@@ -99,13 +117,53 @@ function removeCurrentBook(event) {
     }
 }
 
+// Check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === "") {
+            showError(input);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// Show input error message
+function showError(input) {
+    // takes input parent element
+    const formInput = input.parentElement;
+    // Add error class to current input form-control
+    formInput.className = "form-input error";
+}
+
+// Show success outline
+function showSuccess(input) {
+    const formInput = input.parentElement;
+    formInput.className = "form-input success";
+}
+
 // Function close modal
 function closeModalFunc() {
-    modal.classList.add("hidden");
+    modal.classList.add("modal-hidden");
+    formContainer.reset();
+    resetInputStyles([title, author, pages]);
+
+    smallHidden.classList.add("hidden");
+}
+
+// Reset inputs styles
+function resetInputStyles(inputArr) {
+    inputArr.forEach((input) => {
+        const formInput = input.parentElement;
+        formInput.classList.remove("error");
+        formInput.classList.remove("success");
+    });
 }
 
 // Event Listeners
 addBook.addEventListener("click", addBookToLibrary);
 container.addEventListener("click", removeCurrentBook);
-openModal.addEventListener("click", () => modal.classList.remove("hidden"));
+openModal.addEventListener("click", () =>
+    modal.classList.remove("modal-hidden")
+);
 closeModal.addEventListener("click", closeModalFunc);
